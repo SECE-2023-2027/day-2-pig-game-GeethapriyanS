cfe118dce620a065ebecbd850731a610156f74a5
GeethapriyanS <geethapriyan.s2023cse@sece.ac.in>
2025-07-04 23:29:33 +0530

    changes added

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const diceImg = document.getElementById('diceimages');
-const score1 = document.getElementById('score1');
-const score2 = document.getElementById('score2');
-const current1 = document.getElementById('current1');
-const current2 = document.getElementById('current2');
-const btnNew = document.querySelector('.new-game button');
-const btnRoll = document.querySelector('.roll-dice button');
-const btnHold = document.querySelector('.hold button');
-const player1El = document.querySelector('.player1');
-const player2El = document.querySelector('.player2');
+const diceImg = document.getElementById("diceimages");
+const score1 = document.getElementById("score1");
+const score2 = document.getElementById("score2");
+const current1 = document.getElementById("current1");
+const current2 = document.getElementById("current2");
+const btnNew = document.querySelector(".new-game button");
+const btnRoll = document.querySelector(".roll-dice button");
+const btnHold = document.querySelector(".hold button");
+const player1El = document.querySelector(".player1");
+const player2El = document.querySelector(".player2");
 
-let scores, currentScore, activePlayer, playing,player1,player2;
+let scores, currentScore, activePlayer, playing, player1, player2;
 
 function init() {
-  player1=0;
-  player2=0;
+  player1 = 0;
+  player2 = 0;
   currentScore = 0;
   activePlayer = 0;
   playing = true;
@@ -23,17 +23,18 @@ function init() {
   current1.textContent = 0;
   current2.textContent = 0;
 
-  diceImg.src = './images/dice-1.jpg';
+  diceImg.src = "./images/dice-0.jpeg";
 
-  player1El.style.opacity = '1';
-  player2El.style.opacity = '1';
+  player1El.style.opacity = "1";
+  player2El.style.opacity = "1";
 
-  player1El.style.border = '4px solid black';
-  player2El.style.border = 'none';
+  player1El.style.border = "4px solid black";
+  player2El.style.border = "none";
 }
 
 function switchPlayer() {
   document.getElementById(`current${activePlayer + 1}`).textContent = 0;
+  diceImg.src = "./images/dice-0.jpeg";
   currentScore = 0;
   switch (activePlayer) {
     case 0:
@@ -41,79 +42,80 @@ function switchPlayer() {
       break;
     case 1:
       activePlayer = 0;
-      break;      
+      break;
+  }
+  switch (activePlayer) {
+    case 0:
+      player2El.style.opacity = "0.5";
+      player1El.style.opacity = "1";
+      break;
+    case 1:
+      player1El.style.opacity = "0.5";
+      player2El.style.opacity = "1";
+      break;
   }
 
   switch (activePlayer) {
-    case 0: 
-      player2El.style.opacity = '0.5';
-      player1El.style.opacity = '1';
+    case 0:
+      player2El.style.border = "none";
+      player1El.style.border = "4px solid black";
       break;
     case 1:
-      player1El.style.opacity = '0.5';
-      player2El.style.opacity = '1';
+      player1El.style.border = "none";
+      player2El.style.border = "4px solid black";
       break;
   }
-  
-  switch (activePlayer) {
-    case 0:             
-      player2El.style.border = 'none';
-      player1El.style.border = '4px solid black';
-      break;
-    case 1:
-      player1El.style.border = 'none';
-      player2El.style.border = '4px solid black';
-      break;
-  } 
 }
 
-btnRoll.addEventListener('click', function () {
+function rollDice() {
   if (!playing) {
     return;
   }
+  intervalId = setInterval(() => {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceImg.src = `./images/dice-${dice}.jpg`;
 
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  diceImg.src = `./images/dice-${dice}.jpg`;
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current${activePlayer + 1}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+      clearInterval(intervalId);
+    }
+  }, 1000);
 
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current${activePlayer + 1}`).textContent = currentScore;
-  } else {
-    switchPlayer();
-  }
-});
+}
 
-btnHold.addEventListener('click', function () {
+function hold() {
   if (!playing) {
     return;
   }
-
-  if(activePlayer === 0) {
+  clearInterval(intervalId);
+  if (activePlayer === 0) {
     player1 += currentScore;
-  }
-  else {
+  } else {
     player2 += currentScore;
   }
-  if( activePlayer === 0) {
-    score1.textContent = player1; 
-  }
-  else {
-    score2.textContent = player2; 
+  if (activePlayer === 0) {
+    score1.textContent = player1;
+  } else {
+    score2.textContent = player2;
   }
 
   if (player1 >= 100) {
     playing = false;
-    diceImg.src = '';
+    diceImg.src = "";
     alert(`Player 1 wins!`);
+    init();
   } else if (player2 >= 100) {
     playing = false;
-    diceImg.src = '';
+    diceImg.src = "";
     alert(`Player 2 wins!`);
+    init();
   } else {
     switchPlayer();
   }
-});
-
-btnNew.addEventListener('click', init);
+}
 
 init();
